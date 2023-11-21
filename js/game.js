ba89faeebb7ec47c906c716daef7141f209c1a27
Angel Castro <angelcastro.pr@gmail.com>
2023-11-21 09:57:57 -0400

    Added some scenes

--- js/game.js
+++ js/game.js
@@ -4,57 +4,125 @@ const optionsElement = document.getElementById('options');
 // Initial game state
 let currentScene = 1;
 
-// Define your game scenes
+// Define the game scenes
 const scenes = {
     1: {
-        story: "You find yourself standing in front of a mysterious cave. What will you do?",
+        background: "images/ImpulseZone.png",
+        // story: "You are in a deep sleep. You hear a distant 'Meow!' You decide to...",
         options: [
-            { text: "Enter the cave", nextScene: 2 },
-            { text: "Walk away", nextScene: 3 }
+            { text: "Start Game", nextScene: 2 },
+            // { text: "Stay asleep", nextScene: 3 }
         ]
     },
     2: {
-        story: "As you enter the cave, you see two tunnels. Which one will you choose?",
+        background: "images/scene1.jpg",
+        story: "You are in a deep sleep. You hear a distant 'Meow!' You decide to...",
         options: [
-            { text: "Go left", nextScene: 4 },
-            { text: "Go right", nextScene: 5 }
+            { text: "Wake Up!", nextScene: 3 },
+            { text: "Keep sleeping", nextScene: 5 }
         ]
     },
     3: {
-        story: "You decide not to enter the cave and continue your journey.",
+        background: "images/reference22.png",
+        story: "You scan your apartment, the 'Meow' grows louder and seems to come from the door...",
         options: [
-            { text: "Start over", nextScene: 1 }
+            { text: "Answer the door", nextScene: 4 },
+            { text: "Walk to your balcony", nextScene: 6 }
         ]
     },
     4: {
-        story: "You encounter a friendly dragon in the left tunnel. What will you do?",
+        background: "images/reference16.jpg",
+        story: "You must think you're crazy, but you see a cat looking at you with interest, the feline is wearing some peculiar goggles...",
         options: [
-            { text: "Pet the dragon", nextScene: 6 },
-            { text: "Run away", nextScene: 7 }
+            { text: "Shut the door and go back to sleep!", nextScene: 5 },
+            { text: "Ehh... Hi?", nextScene: 7 },
+            
         ]
     },
     5: {
-        story: "The right tunnel leads to a treasure room. Congratulations, you've found the treasure!",
+        background: "images/gameover.png",
+        story: "I guess you don't know anything about adventures...",
         options: [
             { text: "Start over", nextScene: 1 }
         ]
     },
     6: {
-        story: "The dragon enjoys your company and leads you to a hidden treasure room.",
+        background: "images/reference4.jpg",
+        story: "You hear distant sirens, your floor is high enough to have a clear view of the city.",
         options: [
-            { text: "Claim the treasure", nextScene: 8 }
+            { text: "Go back inside", nextScene: 3 }
         ]
     },
     7: {
-        story: "The dragon gets upset and breathes fire. Unfortunately, it's not a friendly dragon.",
+        background: "images/reference16.jpg",
+        story: "Thet cat asks if he can come in for a glass of milk, you are definitely dreaming but yet... ",
         options: [
-            { text: "Start over", nextScene: 1 }
+            { text: "You close the door and stop imagining things!", nextScene: 3},
+            { text: "You invite him inside", nextScene: 8 },
+            
         ]
     },
     8: {
-        story: "Congratulations! You've successfully completed the adventure and claimed the treasure.",
+        background: "images/reference21.png",
+        story: "The cat complains about the milk not being whole milk. It scans you with its big goggles and asks for the reference code",
         options: [
-            { text: "Play again", nextScene: 1 }
+            { text: "You stare at the cat...", nextScene: 9 }
+        ]
+    },
+    9: {
+        background: "images/reference21.png",
+        story: "You have no idea of what's going on but you blurt out:",
+        options: [
+            { text: "1828", nextScene: 11 },
+            { text: "1771", nextScene: 10 }
+        ]
+    },
+    10: {
+        background: "images/reference21.png",
+        story: "The cat lets out a small 'hmm', you can sense frustration. I says 'You are not the chosen one', as it starts to leave. ",
+        options: [
+            { text: "You let it leave", nextScene: 5 },
+            { text: "Wait! It's 1828", nextScene: 11 }
+        ]
+    },
+    11: {
+        background: "images/reference21.png",
+        story: "The cat nods and pushes a blue button on a small electronic device attached to its neck",
+        options: [
+            { text: "You stare,amused", nextScene: 12 },
+            // { text: "1771", nextScene: 6 }
+        ]
+    },
+    12: {
+        background: "images/reference21.png",
+        story: "You hear loud banging on the door, the cat runs towards the balcony and says 'Hurry, we have no time!'",
+        options: [
+            { text: "Answer the door", nextScene: 13 },
+            { text: "Follow the talking cat", nextScene: 14 }
+        ]
+    },
+    13: {
+        background: "images/detective.png",
+        story: "Stand down! Must execute!",
+        options: [
+            { text: "You freeze without moving an inch", nextScene: 5 },
+            { text: "You start running towards the balcony!", nextScene: 14 }
+        ]
+    },
+    14: {
+        background: "images/catwaiting.png",
+        story: "We need to jump, now! (the cat yells). You hear something getting closer...",
+        options: [
+            { text: "Hesitate", nextScene: 5 },
+            { text: "Jump off the balcony", nextScene: 6 }
+        ]
+    },
+    15: {
+        background: "images/reference21.png",
+        story: "You have no idea of what's going on but you blurt out:",
+        options: [
+            { text: "1828", nextScene: 6 },
+            { text: "1771", nextScene: 6 }
         ]
     }
 };
@@ -62,6 +130,11 @@ const scenes = {
 // Function to update the game state
 function updateGame() {
     const scene = scenes[currentScene];
+
+    // Set the art for the scene
+    document.body.style.backgroundImage = `url('${scene.background}')`
+
+
 
     // Update story text
     storyElement.textContent = scene.story;
@@ -89,4 +162,4 @@ updateGame();
 
 
 
-// what?
+
